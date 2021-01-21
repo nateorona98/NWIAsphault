cb7248acae4d055943410ff025f4e6cab82bb758
nateorona98 <60599423+nateorona98@users.noreply.github.com>
2021-01-20 23:21:26 -0600

    Add files via upload
    Initial commit. This is a rough outline of the site. Footer still needs to be done, navbar needs social icons, another section may be added, media queries need to be added for mobile view(along with adjusting typography sizes) and a scrollspy will be added to allow the user to see which part of the site they are on.

---
+++ index.js
@@ -0,0 +1,45 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    const form = document.getElementById("form");
+    const button = document.getElementById("submit-btn");
+    const status = document.getElementById("form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks! Your message was received and we'll be getting back to you shortly.";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
